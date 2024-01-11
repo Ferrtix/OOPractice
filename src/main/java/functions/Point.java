2544d8bbc2df7b41e7cc33c6db96ce6e32cd4789
Nikita Vasilev <nikitawasilev121@gmail.com>
2024-01-11 17:33:44 +0400

    3.1+3.2

---
+++ src/main/java/functions/Point.java
@@ -0,0 +1,10 @@
+package functions;
+
+public class Point {
+    public final double x;
+    public final double y;
+    public  Point(double x,double y){
+        this.x=x;
+        this.y=y;
+    }
+}
