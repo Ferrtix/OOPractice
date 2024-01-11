4be32fcaab48049253d74f80d81bde695865cb21
Nikita Vasilev <nikitawasilev121@gmail.com>
2024-01-11 16:05:01 +0400

    SQR поправил обратно+тесты

--- src/main/java/functions/SqrFunction.java
+++ src/main/java/functions/SqrFunction.java
@@ -3,6 +3,6 @@ package functions;
 public class SqrFunction implements MathFunction {
     @Override
     public double apply(double x) {
-        return Math.sqrt(x);
+        return x*x;
     }
 }
