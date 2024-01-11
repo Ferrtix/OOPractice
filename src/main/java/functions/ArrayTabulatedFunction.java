32c3f1a3d04242c6ab1134d13e91910c2df41af4
JoYfanCode <semenmel592@gmail.com>
2024-01-11 18:16:23 +0400

    4.1.5 fix

--- src/main/java/functions/ArrayTabulatedFunction.java
+++ src/main/java/functions/ArrayTabulatedFunction.java
@@ -197,22 +197,6 @@ public class ArrayTabulatedFunction extends AbstractTabulatedFunction implements
 
     @Override
     public Iterator<Point> iterator() {
-        return new Iterator<Point>() {
-            private int i = 0;
-
-            @Override
-            public boolean hasNext() {
-                return (i < count);
-            }
-
-            @Override
-            public Point next() {
-                if (hasNext()) {
-                    Point point = new Point(xValues[i], yValues[i]);
-                    i++;
-                    return point;
-                } else throw new NoSuchElementException();
-            }
-        };
+        throw new UnsupportedOperationException();
     }
 }
