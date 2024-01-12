c57cd7a087319b40f57de9f73b915b73f6c9f711
JoYfanCode <semenmel592@gmail.com>
2024-01-12 16:14:33 +0400

    4.2.5

--- src/test/java/operations/TabulatedFunctionOperationServiceTest.java
+++ src/test/java/operations/TabulatedFunctionOperationServiceTest.java
@@ -28,15 +28,15 @@ class TabulatedFunctionOperationServiceTest {
     double[] xValues = {1, 2, 3, 4};
     double[] yValues = {2, 3, 1, 2};
     LinkedListTabulatedFunction list = new LinkedListTabulatedFunction(xValues, yValues);
-    double[] xValues1 = {1, 2, 3, 4};
-    double[] yValues1 = {2, 2, 2, 4};
-    LinkedListTabulatedFunction list1 = new LinkedListTabulatedFunction(xValues1, yValues1);
+    double[] xValues2 = {1, 2, 3, 4};
+    double[] yValues2 = {2, 2, 2, 4};
+    LinkedListTabulatedFunction list1 = new LinkedListTabulatedFunction(xValues2, yValues2);
     TabulatedFunctionOperationService operation = new TabulatedFunctionOperationService();
     @Test
     public void addTest() {
         TabulatedFunction func = operation.Add(list, list1);
         for (int i = 0; i != func.GetCount(); i++) {
-            assertEquals(yValues[i] + yValues1[i], func.getY(i));
+            assertEquals(yValues[i] + yValues2[i], func.getY(i));
         }
     }
 
@@ -44,7 +44,22 @@ class TabulatedFunctionOperationServiceTest {
     void sub() {
         TabulatedFunction func = operation.Sub(list, list1);
         for (int i = 0; i != func.GetCount(); i++) {
-            assertEquals(yValues[i] - yValues1[i], func.getY(i));
+            assertEquals(yValues[i] - yValues2[i], func.getY(i));
+        }
+    }
+    @Test
+    public void mult() {
+        TabulatedFunction func = operation.Mult(list, list1);
+        for (int i = 0; i != func.GetCount(); i++) {
+            assertEquals(yValues[i] * yValues2[i], func.getY(i));
+        }
+    }
+
+    @Test
+    public void div() {
+        TabulatedFunction func = operation.Div(list, list1);
+        for (int i = 0; i != func.GetCount(); i++) {
+            assertEquals(yValues[i] / yValues2[i], func.getY(i));
         }
     }
 }
